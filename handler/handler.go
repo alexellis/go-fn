55a6f10a31e51dc4119bf23c52e98c8098b27cb2
Alex Ellis <alexellis2@gmail.com>
2018-09-29 09:30:38 +0100

    Fix error
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- handler/handler.go
+++ handler/handler.go
@@ -5,7 +5,7 @@ import (
 )
 
 func Handle(w http.ResponseWriter, r *http.Request) {
-	r.Header().Set("Content-Type", "text/plain")
+	r.Header.Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Handled.. OK"))
 }
